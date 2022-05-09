d915e1fab200d9d00a4187a41b2c5492ba9738a5
Eric Matthews <I30147@verisk.com>
2022-05-09 17:17:28 -0400

    Add files

---
+++ src/Api.DoesntWork/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.DoesntWork.Controllers;
+
+[ApiController]
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/weather")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] _summaries = { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+    [ProducesDefaultResponseType]
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                                                      {
+                                                          Date = DateTime.Now.AddDays(index),
+                                                          TemperatureC = Random.Shared.Next(-20, 55),
+                                                          Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+                                                      })
+                         .ToArray();
+    }
+}
---
+++ src/Api.Works/Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning();
+
+builder.Services.AddVersionedApiExplorer(options =>
+                                         {
+                                             options.GroupNameFormat = "'v'VVV";
+                                             options.SubstituteApiVersionInUrl = true;
+                                         });
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
